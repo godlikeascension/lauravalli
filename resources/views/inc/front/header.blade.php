7ac9df8dda7fb32b0d58c9026af58413f90b4c52
godlikeascension <luca.deirossi91@gmail.com>
2025-10-04 13:48:41 +0200

    No video

--- resources/views/inc/front/header.blade.php
+++ resources/views/inc/front/header.blade.php
@@ -9,13 +9,13 @@
                     <img src="/images/logolau.png" data-at2x="/images/logolau.png" style="height: 100px !important;" alt="" class="mobile-logo">
                 </a>
             </div>
-            <div class="col-auto ms-auto md-ms-0 menu-order position-static">
-                <button class="navbar-toggler float-start" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-line"></span>
-                    <span class="navbar-toggler-line"></span>
-                    <span class="navbar-toggler-line"></span>
-                    <span class="navbar-toggler-line"></span>
-                </button>
+{{--            <div class="col-auto ms-auto md-ms-0 menu-order position-static">--}}
+{{--                <button class="navbar-toggler float-start" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-label="Toggle navigation">--}}
+{{--                    <span class="navbar-toggler-line"></span>--}}
+{{--                    <span class="navbar-toggler-line"></span>--}}
+{{--                    <span class="navbar-toggler-line"></span>--}}
+{{--                    <span class="navbar-toggler-line"></span>--}}
+{{--                </button>--}}
 {{--                <div class="collapse navbar-collapse justify-content-center" id="navbarNav">--}}
 {{--                    <ul class="navbar-nav alt-font">--}}
 {{--                        <li class="nav-item"><a href="/" class="nav-link">Home</a></li>--}}
@@ -25,7 +25,7 @@
 {{--                        <li class="nav-item"><a href="/contatti" class="nav-link">Contatti</a></li>--}}
 {{--                    </ul>--}}
 {{--                </div>--}}
-            </div>
+{{--            </div>--}}
 {{--            <div class="col-auto text-end d-none d-sm-flex">--}}
 {{--                <div class="header-icon">--}}
 {{--                    <div class="header-button">--}}
